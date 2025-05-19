138ca65d5d65a5c7aea7c3f1b274f21c6beb10c8
Yonathan Castillo <leothan522@gmail.com>
2025-05-18 20:33:35 -0400

    Correcion Data SQL Inicial

--- database/migrations/2025_05_18_173638_cargar_valores_iniciales_sql.php
+++ database/migrations/2025_05_18_173638_cargar_valores_iniciales_sql.php
@@ -12,9 +12,28 @@ return new class extends Migration
     public function up(): void
     {
         //
-        /*\Illuminate\Support\Facades\DB::unprepared(
-            file_get_contents(storage_path('app/private/sql/init.sql'))
-        );*/
+        \Illuminate\Support\Facades\DB::unprepared(
+            file_get_contents(storage_path('app/private/sql/init_user.sql'))
+        );
+        \Illuminate\Support\Facades\DB::unprepared(
+            file_get_contents(storage_path('app/private/sql/init_estados.sql'))
+        );
+        \Illuminate\Support\Facades\DB::unprepared(
+            file_get_contents(storage_path('app/private/sql/init_niveles.sql'))
+        );
+        \Illuminate\Support\Facades\DB::unprepared(
+            file_get_contents(storage_path('app/private/sql/init_cargos.sql'))
+        );
+        \Illuminate\Support\Facades\DB::unprepared(
+            file_get_contents(storage_path('app/private/sql/init_permisos.sql'))
+        );
+        \Illuminate\Support\Facades\DB::unprepared(
+            file_get_contents(storage_path('app/private/sql/init_seguridad.sql'))
+        );
+        \Illuminate\Support\Facades\DB::unprepared(
+            file_get_contents(storage_path('app/private/sql/init_socios.sql'))
+        );
+
     }
 
     /**
