0f42a107c8e914a8231955fe01474be70eb1af18
Yonathan Castillo <leothan522@gmail.com>
2025-08-14 15:15:28 -0400

    Cambios menores

--- database/migrations/2025_05_20_200416_cargar_init_atletas.php
+++ database/migrations/2025_05_20_200416_cargar_init_atletas.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         //
-        if (env('CARGAR_INIT_EXTRA', true)){
+        if (env('CARGAR_INIT_ATLETAS', true)){
             \Illuminate\Support\Facades\DB::unprepared(
                 file_get_contents(storage_path('app/private/sql/init_atletas.sql'))
             );
