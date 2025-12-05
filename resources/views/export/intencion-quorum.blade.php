8121f3cd812e752601ac8ff25f289e7856a7f565
Yonathan Castillo <leothan522@gmail.com>
2025-12-04 20:18:29 -0400

    Correcion Quorum

--- resources/views/export/intencion-quorum.blade.php
+++ resources/views/export/intencion-quorum.blade.php
@@ -3,8 +3,7 @@
     <tr>
         <th rowspan="2" style="background-color: #F2F2F2; border: 1px solid #404040; text-align: center; vertical-align: center; font-weight: bold">Nº</th>
         <th rowspan="2" style="background-color: #F2F2F2; border: 1px solid #404040; text-align: center; vertical-align: center; font-weight: bold">DEPORTES - MODALIDAD</th>
-        <th colspan="2" style="background-color: #F2F2F2; border: 1px solid #404040; text-align: center; vertical-align: center; font-weight: bold">CLUBES</th>
-        <th style="background-color: #F2F2F2; border: 1px solid #404040; text-align: center; vertical-align: center; font-weight: bold">ATLETAS</th>
+        <th colspan="3" style="background-color: #F2F2F2; border: 1px solid #404040; text-align: center; vertical-align: center; font-weight: bold">CLUBES</th>
     </tr>
     <tr>
         <th style="background-color: #F2F2F2; border: 1px solid #404040; text-align: center; vertical-align: center; font-weight: bold">Fem.</th>
@@ -20,8 +19,8 @@
                 {{ \Illuminate\Support\Str::upper($row->deporte->deporte) }} -
                 {{ \Illuminate\Support\Str::upper($row->categoria) }}</td>
             @php
-                $femenino = \App\Models\ParticipacionDisciplina::where('id_deporte_oficial', $row->id)->sum('femenino');
-                $masculino = \App\Models\ParticipacionDisciplina::where('id_deporte_oficial', $row->id)->sum('masculino');
+                $femenino = \App\Models\ParticipacionDisciplina::where('id_deporte_oficial', $row->id)->whereNotNull('femenino')->where('femenino', '!=', 0)->count();
+                $masculino = \App\Models\ParticipacionDisciplina::where('id_deporte_oficial', $row->id)->whereNotNull('masculino')->where('masculino', '!=', 0)->count();
                 $totalFemenino = $totalFemenino + $femenino;
                 $totalMasculino = $totalMasculino + $masculino;
             @endphp
